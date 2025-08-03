be67ac4fca0e6ce013cf811edd908a29f1fff9cc
VolotDmitriy <volotkovskiy.dmitriy@gmail.com>
2025-08-03 23:05:52 +0300

    feat: add debounce & use for search requests

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,9 +1,30 @@
-import { useContext } from 'react';
+import debounce from 'lodash.debounce';
+import {
+    type ChangeEvent,
+    useCallback,
+    useContext,
+    useRef,
+    useState,
+} from 'react';
 import { SearchContext } from '../../App.tsx';
 import styles from './Search.module.scss';
 
 const Search = () => {
-    const { searchValue, setSearchValue } = useContext(SearchContext);
+    const { setSearchValue } = useContext(SearchContext);
+    const [value, setValue] = useState('');
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const debounceFunc = useCallback(
+        debounce((str: string) => {
+            setSearchValue(str);
+        }, 1000),
+        [],
+    );
+
+    const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value);
+        debounceFunc(e.currentTarget.value);
+    };
 
     return (
         <div className={styles.root}>
@@ -18,10 +39,14 @@ const Search = () => {
             >
                 <path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path>
             </svg>
-            {searchValue && (
+            {value && (
                 <svg
                     className={styles.clearIcon}
-                    onClick={() => setSearchValue('')}
+                    onClick={() => {
+                        setSearchValue('');
+                        setValue('');
+                        inputRef.current?.focus();
+                    }}
                     width="800px"
                     height="800px"
                     viewBox="0 0 512 512"
@@ -50,8 +75,9 @@ const Search = () => {
                 </svg>
             )}
             <input
-                value={searchValue}
-                onChange={(e) => setSearchValue(e.target.value)}
+                ref={inputRef}
+                value={value}
+                onChange={(e) => onChangeInput(e)}
                 className={styles.input}
                 type="text"
                 placeholder="Поиск ..."
